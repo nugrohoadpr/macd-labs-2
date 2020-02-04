3ded97adc0e4f9b4bf0e68d42a695d90a30bbf92
nugrohoadpr <59697384+nugrohoadpr@users.noreply.github.com>
2020-02-04 15:04:22 +0700

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$settings = array(
+    "container" => "gambar",
+    "protocol" => "https",
+    "account_name" => "adibihakapp",
+    "account_key" => "Ymv3HFig7SHGNP6x7ydG9oGKsqbzGONZjp20t3gNj6QAHWPBee0f8mpmTEFDaZOzqLFCtkwmV9UzuTx1Lp+HqQ=="
+);
+?>
