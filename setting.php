c50ce8beeb7456109e2bb63c97ce56008064150a
nugrohoadpr <59697384+nugrohoadpr@users.noreply.github.com>
2020-02-04 14:14:37 +0700

    Delete setting.php

--- setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-$settings = array(
-    "container" => "gambar",
-    "protocol" => "https",
-    "account_name" => "adibihakapp",
-    "account_key" => "fT/cTeBmZVqOCR6hbLf4mePAQ/CdCTD0ZYMLhUd+7FdeJzDNDZ4/bGNPytacR68wVPmUBtkMtH5td2/Ra8nkGw=="
-);
-?>
